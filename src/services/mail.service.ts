e9bd1a58d7b98ce75ab8be22008ee8780b327617
ducnnn <ducnn.230288@gmail.com>
2023-08-11 22:32:16 +0700

    add email contact

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -23,7 +23,7 @@ export class MailService {
 
   async sendUserContact(context: ContactRequestDto) {
     await this.mailerService.sendMail({
-      to: 'langphongmtb@gmail.com',
+      to: process.env.MAIL_FROM,
       from: '"ARI TECHNOLOGY" <' + process.env.MAIL_FROM + '>',
       subject: 'We got a request to Contact',
       template: './contact',
