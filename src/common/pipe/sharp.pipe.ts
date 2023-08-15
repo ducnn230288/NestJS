57ae59c5f742be8384f5cee15e06d58a94c0564b
ducnnn <ducnn.230288@gmail.com>
2023-08-15 23:05:21 +0700

    add email contact

--- src/common/pipe/sharp.pipe.ts
+++ src/common/pipe/sharp.pipe.ts
@@ -3,17 +3,41 @@ import sizeOf from 'image-size';
 import * as path from 'path';
 import * as sharp from 'sharp';
 import * as fs from 'fs';
+import { S3 } from 'aws-sdk';
 
 @Injectable()
-export class SharpPipe implements PipeTransform<Express.Multer.File, Promise<string>> {
-  async transform(image: Express.Multer.File): Promise<string> {
+export class SharpPipe implements PipeTransform<Express.Multer.File, Promise<{ name: string; url: string }>> {
+  async transform(image: Express.Multer.File): Promise<{ name: string; url: string }> {
     const dimensions = await sizeOf(image.buffer);
     const originalName = path.parse(image.originalname).name;
     const filename = Date.now() + '-' + originalName + '.webp';
-    const pathImage = './uploads';
-    fs.mkdirSync(pathImage, { recursive: true });
-    await sharp(image.buffer).resize(dimensions.width).webp({ effort: 3 }).toFile(path.join('uploads', filename));
 
-    return filename;
+    if (process.env.AWS_ACCESS_KEY_ID) {
+      const buffer = await sharp(image.buffer).resize(dimensions.width).webp({ effort: 3 }).toBuffer();
+      return new Promise((resolve, reject) => {
+        new S3({
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        }).upload(
+          {
+            Bucket: process.env.AWS_ACCESS_BUCKET_NAME + '/avata-dev',
+            Key: filename,
+            Body: buffer,
+          },
+          (err, data) => {
+            if (err) {
+              console.log(err);
+              reject(err.message);
+            }
+            resolve({ name: filename, url: data.Location });
+          },
+        );
+      });
+    } else {
+      const pathImage = './uploads';
+      fs.mkdirSync(pathImage, { recursive: true });
+      await sharp(image.buffer).resize(dimensions.width).webp({ effort: 3 }).toFile(path.join('uploads', filename));
+      return { name: filename, url: process.env.DOMAIN + 'files/' + filename };
+    }
   }
 }
